b9f88e3e5ac12278fac87b457560c51b2691801d
Aneesh Maurya <aneesh.maurya@apvtechnologies.com>
2025-05-25 14:05:42 +0530

    change in index,html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewEncapsulation } from '@angular/core';
 import { HeaderComponent } from '../common/header/header.component';
 import { FooterComponent } from '../common/footer/footer.component';
 @Component({
@@ -6,7 +6,8 @@ import { FooterComponent } from '../common/footer/footer.component';
   standalone: true,
   imports: [HeaderComponent,FooterComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent {
 
